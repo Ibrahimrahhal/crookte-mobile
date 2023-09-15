f397dac232f543198baa839f3674a3cd5c7fcc50
Ibrahim Rahhal <ibrahim.rahhal3636@gmail.com>
2023-09-15 13:11:11 +0300

    fixrs

--- src/navigators/menu.tsx
+++ src/navigators/menu.tsx
@@ -1,7 +1,9 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { Text } from "react-native-paper";
-import HomeNavigator from "./home";
-
+import HomeNavigator from "home/navigators/home";
+import t from "home/utils/i18n";
+import { StyleSheet } from "react-native";
+import configs from "configs/index";
 const Drawer = createDrawerNavigator();
 
 export function MenuNavigator() {
@@ -11,9 +13,19 @@ export function MenuNavigator() {
         name="DrawerHome"
         component={HomeNavigator}
         options={{
+          drawerLabel: () => (
+            <Text style={styles.DrawerLabel}>{t("HomeDrawerLabel")}</Text>
+          ),
           headerShown: false,
         }}
       />
     </Drawer.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+  DrawerLabel: {
+    fontWeight: "bold",
+    color: configs.mainColor,
+  },
+});
